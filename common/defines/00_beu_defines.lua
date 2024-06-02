fd1c8111eedc8b7a41b248eb0f3a863362c4196a
Mateus <mateus.fuzetto@hotmail.com>
2024-06-01 21:00:15 -0300

    [NEW] - Better Europa Universalis

---
+++ common/defines/00_beu_defines.lua
@@ -0,0 +1,20 @@
+NDefines.NDiplomacy.MAX_FREE_CITIES = 15
+NDefines.NDiplomacy.JOIN_HRE_DEVELOPMENT_CAP_VASSAL = 300
+NDefines.NDiplomacy.EXCOMMUNICATE_ACTION_MONTHS = 24
+NDefines.NDiplomacy.CRUSADE_TIMEOUT_YEARS = 20
+NDefines.NDiplomacy.TRUCE_YEARS = 5
+NDefines.NDiplomacy.SCALED_TRUCE_YEARS = 5
+NDefines.NDiplomacy.COALITION_YEARS = 15
+
+NDefines.NDiplomacy.HRE_VOTE_NON_MEMBER = -20
+
+NDefines.NCountry.HRE_MAX_RANK = 2
+NDefines.NCountry.HRE_MAX_RANK_ELECTOR = 3
+NDefines.NCountry.MAX_ACTIVE_POLICIES = 9
+
+NDefines.NCountry.POWER_MAX = 1499
+
+NDefines.NMilitary.SLAVE_RAIDS_DURATION = 1825
+
+NDefines.NReligion.MAX_CARDINALS = 49
+NDefines.NReligion.MAX_CARDINALS_PER_COUNTRY = 10
---
+++ descriptor.mod
@@ -0,0 +1,14 @@
+version="1.0"
+tags={
+	"Utilities"
+	"Events"
+	"Balance"
+}
+name="Better Europa Universalis"
+supported_version="v1.37.0.0"
+dependencies={
+	"Governments Expanded"
+	"Missions Expanded"
+	"Europa Expanded"
+	"Beyond Typus 11.5"
+}
